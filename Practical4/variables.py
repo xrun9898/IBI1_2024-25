717f7f7baf2a3cd10c889bd2cdbc9838c1879ed3
xrun9898 <xruns.9898@gmail.com>
2025-04-24 10:10:37 +0800

    update varible.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -4,10 +4,20 @@ c=a+b
 d=90
 e=5
 f=d+e
-#f is longer,c is faster
+if c>f:
+    print("f is longer,c is faster")
+else:
+    print("c is longer,f is faster")
 
-x=True
-y=False
-print(x and y)
-# result is False
- 
+x = True
+y = False
+w = x and y
+print("w:",w)
+'''
+Truth table for w (x and y)
+| x     | y     | w (x and y) | x or y 
+| True  | True  | True        | True
+| True  | False | False       | True
+| False | True  | False       | True
+| False | False | False       | False
+'''
