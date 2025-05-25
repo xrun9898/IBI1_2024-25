7b1f8d96238cc43d0e6d1fdaaf6ce4de96424680
xrun9898 <xruns.9898@gmail.com>
2025-05-25 11:22:05 +0800

    the program was polished

--- Practical13/alignment.py
+++ Practical13/alignment.py
@@ -4,9 +4,9 @@ import blosum as bl
 def load_seq(file):
     return str(next(SeqIO.parse(file, 'fasta')).seq)
 
-human_seq = load_seq('human.fasta')
-mouse_seq = load_seq('mouse.fasta')
-random_seq = load_seq('random.fasta')
+human_seq = load_seq('C:/Users/Xutianrun/Desktop/IBI1_2024-25/Practical13/human.fasta')
+mouse_seq = load_seq('C:/Users/Xutianrun/Desktop/IBI1_2024-25/Practical13/mouse.fasta')
+random_seq = load_seq('C:/Users/Xutianrun/Desktop/IBI1_2024-25/Practical13/random.fasta')
 
 blosum62 = bl.BLOSUM(62)
 def calculate_score(seq1, seq2):
@@ -29,4 +29,3 @@ score3, id3 =calculate_score(random_seq,mouse_seq)
 print(f'The Hamming distance between human and mouse SOD2 sequences is: {score1}, and the percentage of identical amino acids is: {id1}%')
 print(f'The Hamming distance between human SOD2 sequence and random sequence is: {score2}, and the percentage of identical amino acids is: {id2}%')
 print(f'The Hamming distance between mouse SOD2 sequence and random sequence is: {score3}, and the percentage of identical amino acids is: {id3}%')
-
